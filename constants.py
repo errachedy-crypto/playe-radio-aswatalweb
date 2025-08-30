02af806d34ebc02e1013ef51027caa9892bef6d6
errachedy-crypto <errachedy@gmail.com>
2025-08-30 15:12:04 +0100

    تم التحديث بنجاح

--- constants.py
+++ constants.py
@@ -2,3 +2,12 @@
 CURRENT_VERSION = "0.5"
 UPDATE_URL = "https://raw.githubusercontent.com/errachedy-crypto/playe-radio-aswatalweb/main/version.json"
 STATIONS_URL = "https://aswatalweb.com/radio/radio.json"
+
+# --- Themes Configuration ---
+THEMES = {
+    'dark': 'Dark Theme',
+    'light': 'Light Theme',
+}
+
+# --- Additional Configurations ---
+# You can add more configurations here as per your application's needs
